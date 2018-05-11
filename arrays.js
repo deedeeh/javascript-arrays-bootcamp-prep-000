32afdc27a1a979b5eea9943eec102db1e1e3f055
Dina Hafez <dinahafez.8@gmail.com>
2018-05-11 19:37:37 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,8 +6,7 @@ function addElementToBeginningOfArray(arr, el) {
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, el) {
-  arr.unshift(el);
-  return arr;
+  return arr.unshift(el);
 }
 
 function addElementToEndOfArray(arr, el) {
