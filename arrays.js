46142a85ad195c885062a6ffd732e5fe5abdc2fd
Dina Hafez <dinahafez.8@gmail.com>
2018-05-11 19:26:01 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,11 +1,15 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray(arr, el) {
-  var newArray = [...el, arr];
+  var newArray = arr.unshift(el);
   return newArray;
 }
 
 function destructivelyAddElementToBeginningOfArray(arr, el) {
   arr.unshift(el);
   return arr;
+}
+
+function addElementToBeginningOfArray(arr, el) {
+  
 }
